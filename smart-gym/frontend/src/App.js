b81b1c749596040380b5be06edd8d77c9a439df6
KuyaJames724 <jmm0724mc@yahoo.com>
2025-03-31 13:50:36 -0400

    Updated ui, new navbar, included app.js change

--- smart-gym/frontend/src/App.js
+++ smart-gym/frontend/src/App.js
@@ -10,27 +10,39 @@ import ProfileEdit from "./user/ProfileEdit";
 import ChallengeList from "./components/ChallengeList"
 import ChallengeDetail from "./components/ChallengeDetail"
 import UserChallenges from "./components/UserChallenges"
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
         <Routes>
           <Route path="/" element={<LoginSignup />} />
+          
           <Route path="/main" element={<>
+            <Navbar />
             <MainPage />
-            <DailyTip /> 
           </>} />
+          
           <Route path="/calorie-tracker" element={<>
+            <Navbar />
             <CalorieLog />
             <DailyTip />
           </>} />
           <Route path="/workout-log" element={<>
+            <Navbar />
             <WorkoutLog />
             <DailyTip /> 
           </>} />
-          <Route path="/profile" element={<ProfileEdit />} />
-          <Route path="/progress-report" element={<ProgressReport />} />
-
+          
+          <Route path="/profile" element={<>
+            <Navbar />
+            <ProfileEdit />
+          </>} />
+          <Route path="/progress-report" element={<>
+            <Navbar />
+            <ProgressReport />
+          </>} />
+          
           <Route path="/challenges" element={<ChallengeList />} />
           <Route path="/challenge/:id" element={<ChallengeDetail />} />
           <Route path="/my-challenges" element={<UserChallenges />} />
